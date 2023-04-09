a1180d94e2bf7879b64f499291d04488a8b31cd5
Aisha <kalimatovaaisha@mail.ru>
2023-04-09 21:36:31 +0600

    adding documentation

--- src/Main.java
+++ src/Main.java
@@ -62,26 +62,46 @@ public class Main {
                 System.out.println(findGCD(a10, b10));
         }
     }
+    /**
+     * findMin - function find a minimum number from given array
+     *
+     * @param n - length of given array
+     * @param arr - array to work with them
+     * @return - returns minimum number of array
+     */
     public static int findMin(int n, int[] arr){
-        if(n==1) {
+        if(n==1) { // base case
             return arr[0];
         }
         else {
-            int min = findMin(n-1, arr);
+            int min = findMin(n-1, arr); // repeating statement
             return Math.min(min, arr[n-1]);
         }
     }
+    /**
+     * findAvg - function find average of all elements of array
+     *
+     * @param n - length of given array
+     * @param arr - array to work with them
+     * @return - returns average of array
+     */
     public static double findAvg(int n, int[] arr) {
-        double sum = 0;
-        for (int i = 0; i < n; i++) {
+        double sum = 0; // adding a new value
+        for (int i = 0; i < n; i++) { // summing all element of array
             sum += arr[i];
         }
-        return sum / n;
+        return sum / n; // returning result
     }
+    /**
+     * isPrime - function checking is the given number prime or composite
+     *
+     * @param n - number to check
+     * @return - returns true if number prime, false otherwise
+     */
     public static boolean isPrime(int n) {
-        int div = 0;
+        int div = 0; // adding new value
         for (int i = 1; i < n; i++) {
-            if (n % i == 0) {
+            if (n % i == 0) { // checking for divisible
                 div++;
             }
         }
@@ -92,60 +112,106 @@ public class Main {
             return false;
         }
     }
+    /**
+     * findFact - function find factorial of given number
+     *
+     * @param n - number to find factorial
+     * @return - return factorial of number
+     */
     public static int findFact(int n) {
-        if (n == 1) {
+        if (n == 1) { // base case
             return 1;
         }
         else {
-            return n * findFact(n-1);
+            return n * findFact(n-1); // repeating statement
         }
     }
+    /**
+     * findFib - function find fibonacci number
+     *
+     * @param n - fibonacci number to find
+     * @return - returns founded fibonacci number
+     */
     static int findFib(int n) {
-        if (n == 1 || n == 2) {
+        if (n == 1 || n == 2) { // base case
             return 1;
         }
         else {
-            return findFib(n-1) + findFib(n-2);
+            return findFib(n-1) + findFib(n-2); // repeating statement
         }
     }
+    /**
+     * findPow - function find result of power of two given numbers
+     *
+     * @param a - number
+     * @param n - power of number
+     * @return - returns a power of n
+     */
     static int findPow(int a, int n) {
-        if (n == 1) {
+        if (n == 1) { // base case
             return a;
         }
         else {
-            return a * findPow(a, n-1);
+            return a * findPow(a, n-1); // repeating statement
         }
     }
+    /**
+     * getReverse - function return reverse of array with string
+     *
+     * @param n - length of given array
+     * @param arr - array to reverse
+     * @return - reverse string
+     */
     static String findReverse(int n, int[] arr) {
-        if (n == 1) {
+        if (n == 1) { // base case
             return Integer.toString(arr[0]);
         }
         else {
-            return Integer.toString(arr[n-1]) + " " + findReverse(n-1, arr);
+            return Integer.toString(arr[n-1]) + " " + findReverse(n-1, arr); // repeating statement
         }
     }
+    /**
+     * isAllDigits - function that check is all string contain digits
+     *
+     * @param t - given string to check
+     * @return - returns true if string contain all digits, false otherwise
+     */
     static boolean isAllDigits(String t) {
-        if (t.length() == 1) {
+        if (t.length() == 1) { // base case
             return Character.isDigit(t.charAt(0));
         }
         else {
-            return Character.isDigit(t.charAt(0)) && isAllDigits(t.substring(1));
+            return Character.isDigit(t.charAt(0)) && isAllDigits(t.substring(1)); // repeating statement
         }
     }
+    /**
+     * findBin - function find binomial coefficient by two number
+     *
+     * @param n - first bottom argument
+     * @param k - second top argument
+     * @return - returns binomial coefficient of two numbers
+     */
     static int findBin(int n, int k) {
-        if (k == 0 || k == n) {
+        if (k == 0 || k == n) { // base case
             return 1;
         }
         else {
-            return findBin(n-1, k-1) + findBin(n-1, k);
+            return findBin(n-1, k-1) + findBin(n-1, k); // repeating stateent
         }
     }
+    /**
+     * findGCD - function find the greatest common divisor of two numbers
+     *
+     * @param a - first number
+     * @param b - second number
+     * @return - returns gcd of two numbers
+     */
     public static int findGCD(int a, int b) {
-        if (b == 0) {
+        if (b == 0) { // base case
             return a;
         }
         else {
-            return findGCD(b, a%b);
+            return findGCD(b, a%b); // repeating statement
         }
     }
 }
